e9f940ded724d2346faa13ca268a2f0c042f0081
frankcheung0310-a11y <frankcheung0310@gmail.com>
2026-05-16 10:07:18 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+SITE_TITLE = "ResonQ - Deciphering the Quantum Resonance"
+SITE_DESCRIPTION = "A dedicated space for learning, sharing, and advocating for the success of quantum physics and computing."
+SITE_AUTHOR = "Weiqiang Guo"
+BASE_URL = "https://resonq.com" # 部署时请修改为你自己的域名
